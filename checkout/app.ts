a4e71c747aa5ba298dde1560e777eae4aa17ad2b
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-27 08:50:52 +1000

    🎉 Update

--- checkout/app.ts
+++ checkout/app.ts
@@ -1,26 +1,47 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import DynamoDB from "aws-sdk/clients/dynamodb";
 import Stripe from "stripe";
+import axios from "axios";
 
-const TABLE_NAME = process.env.TABLE_NAME as string;
 const STRIPE_KEY = process.env.STRIPE_KEY as string;
+const API_URL = process.env.API_URL as string;
+const API_TOKEN = process.env.API_TOKEN as string;
+
+const SUCCESS_URL = process.env.SUCCESS_URL as string;
+const CANCEL_URL = process.env.CANCEL_URL as string;
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const checkoutId = event.pathParameters?.checkoutId;
+    const productId = event.pathParameters?.productId;
+
+    const {
+        data: {
+            data: { attributes },
+        },
+    } = await axios.get(`${API_URL}/api/products/${productId}?populate=%2A`, { headers: { Authorization: `Bearer ${API_TOKEN}` } });
 
     const stripe = new Stripe(STRIPE_KEY, { apiVersion: "2020-08-27" });
-    const dynamo = new DynamoDB.DocumentClient();
 
-    const response = await dynamo.get({ Key: { checkoutId }, TableName: TABLE_NAME }).promise();
+    // **** I need to add tax for this
 
-    const checkoutConfig = JSON.parse(response.Item?.value);
-    const checkout = await stripe.checkout.sessions.create(checkoutConfig);
-
-    if (!checkout.url)
-        return {
-            statusCode: 500,
-            body: "Error",
-        };
+    const checkout = await stripe.checkout.sessions.create({
+        success_url: SUCCESS_URL,
+        cancel_url: CANCEL_URL,
+        automatic_tax: { enabled: true },
+        customer_creation: "if_required",
+        shipping_address_collection: { allowed_countries: ["AU"] },
+        // shipping_options: attributes.shipping_rates.data.map(({ data: { attributes } }: { data: { attributes: any } }) => ({})),
+        shipping_options: [
+            {
+                shipping_rate_data: {
+                    display_name: "Hello world",
+                    type: "fixed_amount",
+                    delivery_estimate: { minimum: { unit: "business_day", value: 1 }, maximum: { unit: "business_day", value: 10 } },
+                    fixed_amount: { amount: 1500, currency: "AUD" },
+                },
+            },
+        ],
+        line_items: [{ price_data: { currency: "AUD", product_data: { name: "Product 1", description: "This is my product", images: ["this-is-my-image"] }, unit_amount: 1500 } }],
+    });
+    if (!checkout.url) throw Error("Checkout failed");
 
     return {
         statusCode: 302,
