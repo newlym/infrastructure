133563ca3117534760b4d9937cbe467d98566291
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-28 15:22:20 +1000

    🎉 Update

--- run.sh
+++ run.sh
@@ -1,21 +1,2 @@
-export $(cat .env | xargs)
-
-CONFIG="
-$DB_USERNAME
-$DB_PASSWORD
-$STRIPE_KEY
-$CMS_API_URL
-$CMS_API_TOKEN
-$CHECKOUT_SUCCESS_URL
-$CHECKOUT_CANCEL_URL
-
-
-y
-
-
-"
-
-echo "$CONFIG"
-
-# sam build --beta-features
-# sam deploy --guided << CONFIG
+bash -c "sam build --beta-features"
+bash -c "cat .env | sam deploy --guided"
