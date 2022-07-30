b8827b7a3531e6076999efdece85cf2d94014196
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-30 15:57:22 +1000

    🎉 Update

--- checkout-success-email/app.ts
+++ checkout-success-email/app.ts
@@ -5,14 +5,11 @@ import Stripe from "stripe";
 const MAILCHIMP_REGION = process.env.MAILCHIMP_REGION as string;
 const MAILCHIMP_API_KEY = process.env.MAILCHIMP_API_KEY as string;
 const MAILCHIMP_LIST_ID = process.env.MAILCHIMP_LIST_ID as string;
-const MANDRILL_TEMPLATE_NAME = process.env.MANDRILL_TEMPLATE_NAME as string;
+const MANDRILL_TEMPLATE_ID = process.env.MANDRILL_TEMPLATE_ID as string;
 const MANDRILL_API_KEY = process.env.MANDRILL_API_KEY as string;
 
-export async function updateEmail(checkoutSession: Stripe.Checkout.Session, region: string, listId: string, apiKey: string) {
-    const email = checkoutSession.customer_details?.email;
-    if (!email) return;
-
-    const subscribeUrl = `https://${region}.api.mailchimp.com/3.0/lists/{list_id}`;
+export async function subscribeEmail(email: string, region: string, listId: string, apiKey: string) {
+    const subscribeUrl = `https://${region}.api.mailchimp.com/3.0/lists/${listId}`;
     await axios.post(
         subscribeUrl,
         {
@@ -25,30 +22,37 @@ export async function updateEmail(checkoutSession: Stripe.Checkout.Session, regi
         },
         { headers: { Authorization: `auth ${apiKey}` } }
     );
+}
 
-    // const transactionalUrl = "https://mandrillapp.com/api/1.0/messages/send-template";
-    // await axios.post(transactionalUrl, {});
-
-    //     curl -X POST \
-    //   https://mandrillapp.com/api/1.0/messages/send-template \
-    //   -d '{"key":"","template_name":"","template_content":[],"message":{"html":"","text":"","subject":"","from_email":"","from_name":"","to":[],"headers":{},"important":false,"track_opens":false,"track_clicks":false,"auto_text":false,"auto_html":false,"inline_css":false,"url_strip_qs":false,"preserve_recipients":false,"view_content_link":false,"bcc_address":"","tracking_domain":"","signing_domain":"","return_path_domain":"","merge":false,"merge_language":"mailchimp","global_merge_vars":[],"merge_vars":[],"tags":[],"subaccount":"","google_analytics_domains":[],"google_analytics_campaign":"","metadata":{"website":""},"recipient_metadata":[],"attachments":[],"images":[]},"async":false,"ip_pool":"","send_at":""}'
+export async function sendThankYouEmail(email: string, templateId: string, apiKey: string) {
+    const transactionalUrl = "https://mandrillapp.com/api/1.0/messages/send-template";
+    const out = await axios.post(transactionalUrl, {
+        key: apiKey,
+        template_name: templateId,
+        template_content: [],
+        message: { subject: "Thanks For Purchasing", from_email: "notifications@newlym.com", from_name: "NewLym", to: [{ email: "notifications@newlym.com" }] },
+    });
+    console.log(out.data);
 }
 
 export const lambdaHandler = async (event: SQSEvent): Promise<SQSBatchResponse> => {
     const batchItemFailures: { itemIdentifier: string }[] = [];
-    await Promise.all(
-        event.Records.map(
-            (record) =>
-                new Promise(async (resolve) => {
-                    try {
-                        // const checkoutSession: Stripe.Checkout.Session = JSON.parse(JSON.parse(record.body).Message);
-                        await updateEmail({} as any, MAILCHIMP_REGION, MAILCHIMP_LIST_ID, MAILCHIMP_API_KEY);
-                    } catch {
-                        batchItemFailures.push({ itemIdentifier: record.messageId });
-                    }
-                })
-        )
-    );
+    // await Promise.all(
+    //     event.Records.map(
+    //         (record) =>
+    //             new Promise(async (resolve) => {
+    //                 try {
+    //                     // const checkoutSession: Stripe.Checkout.Session = JSON.parse(JSON.parse(record.body).Message);
+    //                     await updateEmail({} as any, MAILCHIMP_REGION, MAILCHIMP_LIST_ID, MAILCHIMP_API_KEY);
+    //                 } catch {
+    //                     batchItemFailures.push({ itemIdentifier: record.messageId });
+    //                 }
+    //             })
+    //     )
+    // );
+
+    // await subscribeEmail("bengrantalbertosborn@gmail.com", MAILCHIMP_REGION, MAILCHIMP_LIST_ID, MAILCHIMP_API_KEY);
+    await sendThankYouEmail("bengrantalbertosborn@gmail.com", MANDRILL_TEMPLATE_ID, MANDRILL_API_KEY);
 
     return {
         batchItemFailures,
