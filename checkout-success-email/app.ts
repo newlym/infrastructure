5dea6ee30f233325b7db46f863ebd4ca50bb3456
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-30 20:56:15 +1000

    🎉 Update

--- checkout-success-email/app.ts
+++ checkout-success-email/app.ts
@@ -9,10 +9,6 @@ import sgClient from "@sendgrid/client";
 // const MANDRILL_TEMPLATE_ID = process.env.MANDRILL_TEMPLATE_ID as string;
 // const MANDRILL_API_KEY = process.env.MANDRILL_API_KEY as string;
 
-const SENDGRID_API_KEY = "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";
-const SENDGRID_LIST_ID = "d2a11c99-e426-4c04-9451-422371be1785";
-const SENDGRID_TEMPLATE_ID = "d-6d921a94d3794b06a679cd2b939e433b";
-
 sgMail.setApiKey(SENDGRID_API_KEY);
 sgClient.setApiKey(SENDGRID_API_KEY);
 
