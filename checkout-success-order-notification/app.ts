b5d890a494e0db38e9d7306bb3f26bb267633fbd
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-30 21:53:33 +1000

    🎉 Update

--- checkout-success-order-notification/app.ts
+++ checkout-success-order-notification/app.ts
@@ -9,31 +9,33 @@ const SENDGRID_LIST_ID = process.env.SENDGRID_LIST_ID as string;
 sgMail.setApiKey(SENDGRID_API_KEY);
 sgClient.setApiKey(SENDGRID_API_KEY);
 
-export async function sendEmail(checkoutSession: Stripe.Checkout.Session) {
+export async function sendEmail(checkoutSession: Stripe.Checkout.Session, listId: string) {
     const titleText = `${checkoutSession.customer_details?.name || ""} ${checkoutSession.amount_total ? "$" + (checkoutSession.amount_total / 100).toFixed(2) + " AUD" : ""}`;
 
     const contacts = await sgClient.request({
         url: `/v3/marketing/contacts/search`,
         method: "POST",
         body: {
-            query: "email LIKE `%`",
+            query: `CONTAINS(list_ids, '${listId}')`,
         },
     });
+    const to = contacts[1].result.map((res: any) => res.email);
 
-    console.log(contacts);
+    const message = JSON.stringify(checkoutSession);
 
-    // await sgMail.send({
-    //     to: email,
-    //     from: "orders@newlym.com",
-    //     subject: titleText,
-    //     text: JSON.stringify(checkoutSession),
-    // });
+    await sgMail.send({
+        to,
+        from: "orders@newlym.com",
+        subject: titleText,
+        text: message,
+        html: message,
+    });
 }
 
 export const lambdaHandler = async (event: SQSEvent): Promise<SQSBatchResponse> => {
     for (const record of event.Records) {
         const checkoutSession: Stripe.Checkout.Session = JSON.parse(JSON.parse(record.body).Message);
-        await sendEmail(checkoutSession);
+        await sendEmail(checkoutSession, SENDGRID_LIST_ID);
     }
 
     return {
