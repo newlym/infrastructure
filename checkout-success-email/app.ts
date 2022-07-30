cab799e747c58a79ec50b2334c77696982120250
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-30 14:15:23 +1000

    🎉 Update

--- checkout-success-email/app.ts
+++ checkout-success-email/app.ts
@@ -1,31 +1,48 @@
 import { SQSBatchResponse, SQSEvent } from "aws-lambda";
+import axios from "axios";
 import Stripe from "stripe";
-import { Client } from "@notionhq/client";
 
-const NOTION_KEY = process.env.NOTION_KEY as string;
-const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID as string;
+const MAILCHIMP_REGION = process.env.MAILCHIMP_REGION as string;
+const MAILCHIMP_API_KEY = process.env.MAILCHIMP_API_KEY as string;
+const MAILCHIMP_LIST_ID = process.env.MAILCHIMP_LIST_ID as string;
+const MANDRILL_TEMPLATE_NAME = process.env.MANDRILL_TEMPLATE_NAME as string;
+const MANDRILL_API_KEY = process.env.MANDRILL_API_KEY as string;
 
-export async function notifyOrder(checkoutSession: Stripe.Checkout.Session, notion: Client, databaseId: string) {
-    const titleText = `${checkoutSession.customer_details?.name || ""} ${checkoutSession.amount_total ? "$" + (checkoutSession.amount_total / 100).toFixed(2) + " AUD" : ""}`;
+export async function updateEmail(checkoutSession: Stripe.Checkout.Session, region: string, listId: string, apiKey: string) {
+    const email = checkoutSession.customer_details?.email;
+    if (!email) return;
 
-    await notion.pages.create({
-        parent: { database_id: databaseId },
-        properties: { Name: [{ text: { content: titleText } }], Status: { name: "Backlog" } },
-        children: [{ paragraph: { rich_text: [{ text: { content: JSON.stringify(checkoutSession) } }] } }],
-    });
+    const subscribeUrl = `https://${region}.api.mailchimp.com/3.0/lists/{list_id}`;
+    await axios.post(
+        subscribeUrl,
+        {
+            members: [
+                {
+                    email_address: email,
+                    status: "subscribed",
+                },
+            ],
+        },
+        { headers: { Authorization: `auth ${apiKey}` } }
+    );
+
+    // const transactionalUrl = "https://mandrillapp.com/api/1.0/messages/send-template";
+    // await axios.post(transactionalUrl, {});
+
+    //     curl -X POST \
+    //   https://mandrillapp.com/api/1.0/messages/send-template \
+    //   -d '{"key":"","template_name":"","template_content":[],"message":{"html":"","text":"","subject":"","from_email":"","from_name":"","to":[],"headers":{},"important":false,"track_opens":false,"track_clicks":false,"auto_text":false,"auto_html":false,"inline_css":false,"url_strip_qs":false,"preserve_recipients":false,"view_content_link":false,"bcc_address":"","tracking_domain":"","signing_domain":"","return_path_domain":"","merge":false,"merge_language":"mailchimp","global_merge_vars":[],"merge_vars":[],"tags":[],"subaccount":"","google_analytics_domains":[],"google_analytics_campaign":"","metadata":{"website":""},"recipient_metadata":[],"attachments":[],"images":[]},"async":false,"ip_pool":"","send_at":""}'
 }
 
 export const lambdaHandler = async (event: SQSEvent): Promise<SQSBatchResponse> => {
-    const notion = new Client({ auth: NOTION_KEY });
-
     const batchItemFailures: { itemIdentifier: string }[] = [];
     await Promise.all(
         event.Records.map(
             (record) =>
                 new Promise(async (resolve) => {
                     try {
-                        const checkoutSession: Stripe.Checkout.Session = JSON.parse(JSON.parse(record.body).Message);
-                        await notifyOrder(checkoutSession, notion, NOTION_DATABASE_ID);
+                        // const checkoutSession: Stripe.Checkout.Session = JSON.parse(JSON.parse(record.body).Message);
+                        await updateEmail({} as any, MAILCHIMP_REGION, MAILCHIMP_LIST_ID, MAILCHIMP_API_KEY);
                     } catch {
                         batchItemFailures.push({ itemIdentifier: record.messageId });
                     }
@@ -37,3 +54,5 @@ export const lambdaHandler = async (event: SQSEvent): Promise<SQSBatchResponse> 
         batchItemFailures,
     };
 };
+
+lambdaHandler({} as any);
